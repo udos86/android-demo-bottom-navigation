f89544048ff2138262a53fb9758cbf65b53c5e57
Udo Schöfer <developer@udos86.de>
2017-01-21 12:46:11 +0100

    fragment samples added

---
+++ app/src/main/java/de/udos/android_demo_bottom_navigation/ContentFragment1.java
@@ -0,0 +1,29 @@
+package de.udos.android_demo_bottom_navigation;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContentFragment1 extends Fragment {
+
+
+    public ContentFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_content_1, container, false);
+    }
+
+}
--- app/src/main/java/de/udos/android_demo_bottom_navigation/MainActivity.java
+++ app/src/main/java/de/udos/android_demo_bottom_navigation/MainActivity.java
@@ -1,13 +1,67 @@
 package de.udos.android_demo_bottom_navigation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final Fragment currentFragment = fragmentManager.findFragmentById(R.id.containerContentFragment);
+        final Fragment nextFragment;
+
+        switch (item.getItemId()) {
+
+            case R.id.one:
+
+                nextFragment = new ContentFragment1();
+                break;
+
+            case R.id.two:
+
+                nextFragment = new ContentFragment2();
+                break;
+
+            case R.id.three:
+
+                nextFragment = new ContentFragment3();
+                break;
+
+            default:
+
+                nextFragment = null;
+                break;
+        }
+
+        if (currentFragment == null || (currentFragment.getClass() != nextFragment.getClass())) {
+
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.containerContentFragment, nextFragment)
+                    .addToBackStack(null)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .commit();
+        }
+
+        return false;
     }
 }
